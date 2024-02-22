e7a6757d1de3c51ef670b95a53a4d2d8bcf1c245
NikoCyber <postoronnii@ymail.com>
2024-02-22 16:29:51 -0500

    1st task

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,44 @@
+import { useEffect, useRef, useState } from 'react'
+import Counter from './components/CounterClass'
+import Genre from './components/Genre'
+import SearchForm from './components/SearchForm'
+
 export default function App() {
-  return <div>Hello world!</div>
+  const movies = ['all', 'documentary', 'comedy', 'horror', 'crime']
+
+  const [selectedGenre, setSelectedGenre] = useState(null)
+  const [search, setSearch] = useState('')
+  const inputRef = useRef(null)
+
+  const handleSelectedGenre = (genre) => {
+    setSelectedGenre(genre)
+  }
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+    if (inputRef.current.value === '') return
+    setSearch(inputRef.current.value)
+  }
+
+  useEffect(() => {
+    console.log(search)
+  }, [search])
+
+  return (
+    <div>
+      <div className="flex justify-center items-center ">
+        <Counter value={0} />
+      </div>
+      <div className="flex justify-center items-center pt-10">
+        <SearchForm onSearch={handleSearch} inputRef={inputRef} />
+      </div>
+      <div className="flex justify-center items-center pt-10">
+        <Genre
+          genres={movies}
+          selectedGenre={selectedGenre}
+          onSelect={handleSelectedGenre}
+        />
+      </div>
+    </div>
+  )
 }
