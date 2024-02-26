b308158eea310052431d3d15de62234016c987dc
NikoCyber <postoronnii@ymail.com>
2024-02-26 09:03:54 -0500

    Font updated

--- src/components/Genre.jsx
+++ src/components/Genre.jsx
@@ -1,14 +1,14 @@
 export default function Genre({ genres, selectedGenre, onSelect }) {
   return (
     <div>
-      <div className="flex flex-wrap gap-3 items-center justify-between max-w-7xl m-auto text-xl">
+      <div className="flex flex-wrap items-center justify-between max-w-7xl gap-5 m-auto text-xl text-gray-500 font-bold">
         {genres.map((genre, index) => {
           return (
             <button
               key={index}
-              className={`w-32 ${
-                selectedGenre === index ? 'font-bold' : 'font-normal'
-              }`}
+              className={`${
+                selectedGenre === index ? 'text-black' : 'text-gray-500'
+              } `}
               onClick={() => onSelect(index)}
             >
               {genre.toUpperCase()}
